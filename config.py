c971c4b19505abb590be9b613267cf118d28347f
Angelito <tachiangelito@gmail.com>
2026-03-03 05:20:15 -0600

    seguridad

--- config.py
+++ config.py
@@ -8,8 +8,11 @@ SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = 'Lax' 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretke291762y")
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://Angelito:Angelito05@cluster0.ifetmab.mongodb.net/")
-    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME", "dgijgaoyp")
-    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY", "367923394437628")
-    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET", "Q3Lj95oGK2FXaKUu_dfo-j5cNLI")
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    MONGODB_URI = os.environ.get('MONGODB_URI')
+    
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
